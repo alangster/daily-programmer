29472b8edf302377c499bb039cdc6882ab264490
Alex Lang <langalex4@gmail.com>
2015-02-02 12:32:18 -0600

    able to translate input

--- bank-number-banner/number_banner_pt_2.rb
+++ bank-number-banner/number_banner_pt_2.rb
@@ -15,12 +15,27 @@ BANNER_ARR = [
 
 def decipher(chunk_arr)
 	possible_nums = Array(0..9)
-
 	chunk_arr.each.with_index do |chunk, i|
 		possible_nums = possible_nums & BANNER_ARR[i].map.with_index {|segment, i| i if segment == chunk}
 		break if possible_nums.count == 1
 	end
-
 	possible_nums[0]
 
 end
+
+def make_segments(input_arr)
+	segments_array = input_arr.map {|str| str.scan(/[_\|\s]{4}/).map(&:chop)}
+	(0..(segments_array[0].count - 1)).map do |place|
+		segments_array.map {|seg| seg[place]}
+	end
+end
+
+def translate(input)
+	make_segments(input).map {|chunk| decipher(chunk)}.join
+end
+
+input = ["     _   _   _   _   _   _       _  ",
+				 "|_| |_| | | | | |_    |   |  |  |_  ",
+				 "  |  _| |_| |_| |_|   |   |  |   _| "]
+
+puts translate(input)
